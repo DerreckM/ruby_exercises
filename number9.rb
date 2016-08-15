f987c8a381ae227d832c4f20297038e16c79e5aa
Derreck Mansheim <mansheim23@hotmail.com>
2016-08-15 09:27:25 -0500

    solution to number9.rb

--- number9.rb
+++ number9.rb
@@ -3,3 +3,48 @@
 # At the end the number of tries needed should be printed. I counts only as one try
 # if they input the same number multiple times consecutively.
 
+puts "Welcome to The Guessing Game!"
+
+keepplaying = "y"
+
+while keepplaying == "y"
+
+		win = false
+    guesscount = 0
+
+		randNum = rand(100) + 1
+
+		puts "Please enter a number (1-100) or -1 to exit: "
+
+		# Keep prompting for numbers and comparing.
+		while !win
+
+				# obtain number from user
+				guessNumber = gets.to_i
+
+				# Wants to quit
+				if guessNumber == -1 then
+						break
+				end
+
+				# Compare number to computer number
+				if guessNumber > randNum then
+						puts "You guessed too high, please try again."
+            guesscount += 1
+				elsif guessNumber < randNum then
+						puts "You guessed too low, please try again."
+            guesscount += 1
+				else
+						puts "Congratulations, you win!"
+						win = true
+	          print "You took " + guesscount.to_s + " tries. "
+				end
+		end
+
+		# If game has ended, ask the user to play again.
+		puts "Want to play again? (y/n)"
+		keepplaying = gets.chomp!
+end
+
+puts "Thanks for playing The Guessing Game!"
+
