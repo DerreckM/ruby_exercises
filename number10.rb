0416be392f6df98b1f1fedd2e27965fa000944a5
Derreck Mansheim <mansheim23@hotmail.com>
2016-08-16 15:54:02 -0500

    added Time class to leap year solution

--- number10.rb
+++ number10.rb
@@ -1,16 +1,16 @@
 # 10. Write a program that prints the next 20 leap years.
-
+require 'time'
 # We know 2016 was a leap year, so start the counter for next year
-startYear = 2017
+startYear = Time.now.year
 
 #We know a leap year is every four years, so 20 leap years is 80 years
 endYear = startYear + 80
 
 while startYear <= endYear
-
-  if ((startYear % 4 == 0) && (startYear % 100 != 0)) || (startYear %
-400 == 0)
-    print startYear.to_i.split(',')
+  if Date.leap?(startYear)
+  #if ((startYear % 4 == 0) && (startYear % 100 != 0)) || (startYear %
+#400 == 0)
+    print startYear.to_s.split(", ")
   end
   startYear = startYear + 1
 end
